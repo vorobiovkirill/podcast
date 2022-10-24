edf3fc783afb2dd1cb1cd56955a8f5a90f3d8e4b
Kirill Vorobiov <kvorobiov@vareger.com>
2022-10-24 18:08:42 +0300

    del secrets

--- app/libs/supabase-client.ts
+++ app/libs/supabase-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL as string || 'https://ifndmjhlubaqtrhdwxaa.supabase.co';
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbmRtamhsdWJhcXRyaGR3eGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1ODIxMDAsImV4cCI6MTk2MDE1ODEwMH0.ypv_LTlXP_RBGG_qLf3ioMxFq9PuTXQF2SVeBBTClLo';
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+
+console.log('process.env', process.env)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
